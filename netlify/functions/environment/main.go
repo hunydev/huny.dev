db6bcff69c17935416d5649a3c2dc77a3cdcebf2
Janghun <66767008+hunydev@users.noreply.github.com>
2024-04-20 14:49:59 +0000

    path 변경

--- netlify/functions/environment/main.go
+++ netlify/functions/environment/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -9,12 +8,6 @@ import (
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	if request.HTTPMethod != "POST" {
-		return &events.APIGatewayProxyResponse{
-			StatusCode: http.StatusMethodNotAllowed,
-		}, nil
-	}
-
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       os.Getenv("ADMINISTRATOR"),
