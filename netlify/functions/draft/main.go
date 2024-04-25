5f7ef9330f4e5eafcbec51a6759b5e0a5583b127
Janghun <66767008+hunydev@users.noreply.github.com>
2024-04-25 15:40:56 +0000

    like function

--- netlify/functions/draft/main.go
+++ netlify/functions/draft/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -25,17 +28,71 @@ var query string = `{
     ]
 }`
 
+type PropertyNumber struct {
+	Object    string `json:"object"`
+	Type      string `json:"number"`
+	ID        string `json:"id"`
+	Number    int    `json:"number"`
+	RequestID string `json:"request_id"`
+}
+
+func addLike(id string, v int) error {
+	notionAPIEndpoint := fmt.Sprintf("https://api.notion.com/v1/pages/%s/properties/fRbV", id)
+	notionVersion := "2022-06-28"
+	notionAPIKey := os.Getenv("NOTION_API_KEY")
+
+	req, err := http.NewRequest("GET", notionAPIEndpoint, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Notion-Version", notionVersion)
+	req.Header.Set("Authorization", "Bearer "+notionAPIKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return errors.New(resp.Status)
+	}
+
+	like := &PropertyNumber{}
+	if err := json.NewDecoder(resp.Body).Decode(like); err != nil {
+		return err
+	}
+
+	number := fmt.Sprintf(`{
+		"properties": {
+			"Like": {
+				"number": %d
+			}
+		}
+	}`, like.Number+v)
+
+	notionAPIEndpoint = fmt.Sprintf("https://api.notion.com/v1/pages/%s", id)
+
+	req, err = http.NewRequest("PATCH", notionAPIEndpoint, strings.NewReader(number))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Notion-Version", notionVersion)
+	req.Header.Set("Authorization", "Bearer "+notionAPIKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func get(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-
-	return nil, nil
-}
-
-func update(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-
-	return nil, nil
-}
-
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	notionAPIEndpoint := "https://api.notion.com/v1/databases/f1eee6b74999424fb7de5b6e2e5367a0/query"
 	notionVersion := "2022-06-28"
 	notionAPIKey := os.Getenv("NOTION_API_KEY")
@@ -65,6 +122,38 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 		StatusCode: 200,
 		Body:       string(buf),
 	}, nil
+}
+
+func patch(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	id := request.QueryStringParameters["id"]
+	action := request.QueryStringParameters["action"]
+
+	if id == "" || action == "" {
+		return &events.APIGatewayProxyResponse{StatusCode: http.StatusAccepted, Body: ""}, nil
+	}
+
+	var err error
+	if action == "like" {
+		err = addLike(id, 1)
+	} else if action == "unlike" {
+		err = addLike(id, -1)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &events.APIGatewayProxyResponse{StatusCode: http.StatusOK, Body: action}, nil
+}
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch request.HTTPMethod {
+	case "GET", "get":
+		return get(request)
+	case "PATCH", "patch":
+		return patch(request)
+	}
+
+	return &events.APIGatewayProxyResponse{StatusCode: http.StatusMethodNotAllowed}, nil
 }
 
 func main() {
